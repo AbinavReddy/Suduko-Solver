05be5f5f73eeed648e7dfaca46c50dddea885cdd
DBorke <dannybborke@gmail.com>
2024-02-21 13:47:43 +0100

    Made constructor initialize variables and created initializeBoard method.

--- src/BasicSudoku/Board.java
+++ src/BasicSudoku/Board.java
@@ -2,16 +2,28 @@ package BasicSudoku;
 
 public class Board
 {
+    private int boardSize;
     private int[][] board;
-    private int boardSize = 9;
     private int availableCells;
     private int filledCells = 0;
 
-    public Board (){
+    public Board(int boardSize)
+    {
+        this.boardSize = boardSize;
+
         board = new int [boardSize][boardSize];
 
-        for(int row = 0; row<= boardSize;row++){
-            for(int column = 0; column <= boardSize; column++){
+        availableCells = boardSize * boardSize;
+
+        initializeBoard();
+    }
+
+    public void initializeBoard()
+    {
+        for(int row = 0; row <= boardSize; row++)
+        {
+            for(int column = 0; column <= boardSize; column++)
+            {
                 board[row][column] = 0;
             }
         }
