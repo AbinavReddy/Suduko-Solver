30001728b5ffbab85ab989af3f8c9ad68a7f94ce
Abinav <abinavaleti18@gmail.com>
2024-02-21 14:23:37 +0100

    Add findSubBoardNumber

--- src/BasicSudoku/Board.java
+++ src/BasicSudoku/Board.java
@@ -1,10 +1,12 @@
 package BasicSudoku;
 
+import java.lang.Math;
+import java.util.*;
 public class Board
 {
     private int n = 9;
     private int[][] board;
-    private int boardSize = 9;
+    private int boardSize = n;
     private int availableCells;
     private int filledCells = 0;
 
@@ -16,6 +18,12 @@ public class Board
                 board[row][column] = 0;
             }
         }
+    }
+
+    public int findSubBoardNumber(int row,int column)
+    {
+        int totalNoOfSubBoards = (int) Math.sqrt(boardSize);
+        return (row/totalNoOfSubBoards)*totalNoOfSubBoards + (column/totalNoOfSubBoards)+1;
     }
 
 }
