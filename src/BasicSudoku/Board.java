14e7d44149245662d76cf10fc0f517f12b6e3bbf
Abinav <abinavaleti18@gmail.com>
2024-02-21 13:25:22 +0100

    Added Constructor for board

--- src/BasicSudoku/Board.java
+++ src/BasicSudoku/Board.java
@@ -4,8 +4,18 @@ public class Board
 {
     private int n = 9;
     private int[][] board;
-    private int boardSize = n;
+    private int boardSize = 9;
     private int availableCells;
     private int filledCells = 0;
+
+    public Board (){
+        board = new int [boardSize][boardSize];
+
+        for(int row = 0; row<= boardSize;row++){
+            for(int column = 0; column <= boardSize; column++){
+                board[row][column] = 0;
+            }
+        }
+    }
 
 }
