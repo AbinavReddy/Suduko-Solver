67bca9dbd9c5acbd84ba368cb17ab87184bf3b74
Abinav <abinavaleti18@gmail.com>
2024-02-21 13:16:06 +0100

    Added feilds in board class

--- src/BasicSudoku/Board.java
+++ src/BasicSudoku/Board.java
@@ -2,5 +2,10 @@ package BasicSudoku;
 
 public class Board
 {
+    private int n = 9;
+    private int[][] board;
+    private int boardSize = n;
+    private int availableCells;
+    private int filledCells = 0;
 
 }
