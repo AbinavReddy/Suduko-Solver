921aeffd905605622428c0ce68924f1b7caa4975
AbinavReddy <122014198+AbinavReddy@users.noreply.github.com>
2024-04-17 13:34:18 +0200

    added gui part

--- src/BasicSudoku/Main.java
+++ src/BasicSudoku/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
@@ -19,12 +20,15 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("SudokuBoard.fxml")); // Load FXML
             Parent root = loader.load(); // Set FXML as root
             Scene scene = new Scene(root); // Set root for scene
+            SudokuApp controller = loader.getController();
+            controller.setPrimaryStage(primaryStage);
             primaryStage.getIcons().addAll( // Add icons to stage
                     new Image(getClass().getResourceAsStream("sudoku icon.png")));
             primaryStage.setResizable(true); // Disable resizable window
             primaryStage.setTitle("Sudoku"); // Window title
             primaryStage.setScene(scene); // Construct scene
             primaryStage.show(); // Show window
+
 
         } catch(Exception e) {
             e.printStackTrace();
@@ -36,4 +40,9 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 }
+
+
--- src/BasicSudoku/SudokuApp.java
+++ src/BasicSudoku/SudokuApp.java
@@ -1,6 +1,75 @@
 package BasicSudoku;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
 public class SudokuApp
 {
+
+    Board board;
+
+    @FXML
+    private TextField textField;
+
+    private Stage primaryStage;
+
+    public void setPrimaryStage(Stage primaryStage){
+        this.primaryStage = primaryStage;
+    }
+
+
+
+
+    @FXML
+private void setBoard(ActionEvent event){
+        // abinav
+        String text = textField.getText();
+        System.out.println(text);
+        if(Integer.parseInt(text) > 0){
+            board = new Board(Integer.parseInt(text), false, 30, false);
+            changeScene(primaryStage);
+        }
+    }
+
+    private void changeScene(Stage stage){
+        // abinav
+        GridPane gp = new GridPane();
+
+        for (int row = 0; row < board.getBoardSize(); row++) {
+            for (int col = 0; col < board.getBoardSize() ; col++) {
+                TextField cells = new TextField();
+                cells.setPrefSize(30,30);
+               cells.setStyle(
+                        "-fx-font-size: 16px; " +
+                                "-fx-font-family: 'Arial'; " +
+                                "-fx-border-color: #333; " +
+                                "-fx-border-width: 1px; " +
+                                "-fx-background-color: #fff; " +
+                                "-fx-text-fill: #666; " +
+                                "-fx-padding: 5px;"
+                );
+                int value = board.getBoard()[row][col];
+                if ( value == 0){
+                    cells.setEditable(true);
+                    cells.setPromptText("");
+                }else {
+                    cells.setPromptText(Integer.toString(value));
+                    cells.setEditable(false);
+                }
+                gp.add(cells,col,row);
+            }
+        }
+        Scene newScene = new Scene(gp,1300,1200);
+        stage.setScene(newScene);
+        stage.show();
+    }
+
 
 }
