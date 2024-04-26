3527f4178f6399b71e8edc5bd07177efe7521f1e
DBorke <dannybborke@gmail.com>
2024-04-26 10:51:25 +0200

    Moved all UI-elements to their own folder.

--- src/BasicSudoku/Main.java
+++ src/BasicSudoku/Main.java
@@ -5,12 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import javafx.application.Platform;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ButtonType;
 
 public class Main extends Application {
 
@@ -19,13 +14,13 @@ public class Main extends Application {
      */
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("SudokuBoard.fxml")); // Load FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("UI/MenuScene.fxml")); // Load FXML
             Parent root = loader.load(); // Set FXML as root
             Scene scene = new Scene(root); // Set root for scene
             SudokuApp controller = loader.getController();
             controller.setPrimaryStage(primaryStage);
             primaryStage.getIcons().addAll( // Add icons to stage
-                    new Image(getClass().getResourceAsStream("sudoku icon.png")));
+                    new Image(getClass().getResourceAsStream("UI/sudoku icon.png")));
             primaryStage.setResizable(true); // Disable resizable window
             primaryStage.setTitle("Sudoku"); // Window title
             primaryStage.setScene(scene); // Construct scene
